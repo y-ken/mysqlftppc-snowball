9b08dc221e085654d7dbb0d08affe68c7b9caebc
hiroaki-kawai <hiroaki-kawai@fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6>
2009-01-26 08:03:08 +0000

    add missing initialization.
    git-svn-id: http://mysqlftppc.svn.sourceforge.net/svnroot/mysqlftppc/snowball/trunk@106 fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6

--- plugin_snowball.c
+++ plugin_snowball.c
@@ -201,7 +201,7 @@ static size_t str_convert(CHARSET_INFO *cs, char *from, size_t from_length,
                           size_t *numchars){
   char *rpos, *rend, *wpos, *wend;
   my_wc_t wc;
-  char* tmp;
+  char* tmp = NULL;
   
   if(numchars){ *numchars = 0; }
   rpos = from;
