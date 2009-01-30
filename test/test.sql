eb0bf6d18568bf2247ca34486029cac17de30804
hiroaki-kawai <hiroaki-kawai@fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6>
2009-01-30 09:35:37 +0000

    add simple test script. the scripts reset plugin system variables.
    git-svn-id: http://mysqlftppc.svn.sourceforge.net/svnroot/mysqlftppc/snowball/trunk@119 fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6

---
+++ test/test.sql
@@ -0,0 +1,26 @@
+﻿SET GLOBAL snowball_normalization=OFF;
+SET GLOBAL snowball_unicode_version="DEFAULT";
+SET GLOBAL snowball_algorithm="english";
+
+DROP TABLE IF EXISTS sn;
+CREATE TABLE sn (a TEXT, FULLTEXT(a) WITH PARSER snowball) CHARSET latin1;
+INSERT INTO sn VALUES ("consignment consistently");
+INSERT INTO sn VALUES ("dummy");
+INSERT INTO sn VALUES ("dummy");
+SELECT COUNT(*) FROM sn WHERE MATCH(a) AGAINST('consigned consisted');
+SELECT COUNT(*) FROM sn WHERE MATCH(a) AGAINST('consigned' IN BOOLEAN MODE);
+DROP TABLE IF EXISTS sn;
+
+SET GLOBAL snowball_algorithm="fr";
+DROP TABLE IF EXISTS sn;
+CREATE TABLE sn (a TEXT, FULLTEXT(a) WITH PARSER snowball) CHARSET latin1;
+INSERT INTO sn VALUES ("continué continuellement");
+INSERT INTO sn VALUES ("dummy");
+INSERT INTO sn VALUES ("dummy");
+SELECT COUNT(*) FROM sn WHERE MATCH(a) AGAINST('continuait continuelle');
+SELECT COUNT(*) FROM sn WHERE MATCH(a) AGAINST('continuait' IN BOOLEAN MODE);
+DROP TABLE IF EXISTS sn;
+
+SET GLOBAL snowball_normalization=OFF;
+SET GLOBAL snowball_unicode_version="DEFAULT";
+SET GLOBAL snowball_algorithm="english"
